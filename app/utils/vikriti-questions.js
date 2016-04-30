334b14687245a715fe6c2e0b2ffdbd731c144761
Kathleen Moynahan <mary13@gmail.com>
2016-04-30 12:49:14 -0700

    updates questions

--- app/utils/vikriti-questions.js
+++ app/utils/vikriti-questions.js
@@ -2,440 +2,440 @@ export default [
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'sex',
     text: 'noticed that my libido has increased or decreased dramatically.'
   },
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'body',
     text: 'been feeling restless, fidgety, and my spine or limbs have felt stiff.'
   },
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'skin',
     text: 'had dry skin or hair.'
   },
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'circulation',
     text: 'had cold hands and/or feet.'
   },
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'manner',
     text: 'been very easily startled by loud noises.'
   },
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'weight',
     text: 'had others told me that I am underweight and/or I have felt that I could gain some weight.'
   },
   {
     dosha: 'vata',
     section: 'physical',
-    type: '',
+    type: 'energy',
     text: 'felt fatigued or exhausted, especially in the afternoon.'
   },
   {
     dosha: 'vata',
-    type: '',
+    type: 'temperature',
     section: 'physical',
     text: 'had increased senstitivity to wind and cold.'
   },
   {
     dosha: 'vata',
-    type: '',
+    type: 'eyes',
     section: 'physical',
     text: 'noticed that my hands are shaky or my eyes twitch.'
   },
   {
     dosha: 'vata',
     section: 'mental',
-    type: '',
+    type: 'anxious',
     text: 'felt anxious, worried, and ungrounded.'
   },
   {
     dosha: 'vata',
     section: 'mental',
-    type: '',
+    type: 'sleep',
     text: 'been unable to fall and/or stay soundly asleep.'
   },
   {
     dosha: 'vata',
     section: 'mental',
-    type: '',
+    type: 'mind',
     text: 'had difficulty focusing and my memory is not as strong as usual.'
   },
   {
     dosha: 'vata',
     section: 'mental',
-    type: '',
+    type: 'feeling',
     text: 'often felt isolated and alone.'
   },
   {
     dosha: 'vata',
     section: 'mental',
-    type: '',
+    type: 'worry',
     text: 'been waking up early in the morning feeling anxious, panicked, or with racing thoughts.'
   },
   {
     dosha: 'vata',
     section: 'digestive',
-    type: '',
-    text: 'been constipated or my elimination has become irregular. '
+    type: 'bowels',
+    text: 'been constipated or my elimination has become irregular.'
   },
   {
     dosha: 'vata',
     section: 'digestive',
-    type: '',
+    type: 'digestion',
     text: 'been experiencing gas and bloating.'
   },
   {
     dosha: 'vata',
     section: 'digestive',
-    type: '',
+    type: 'elimination',
     text: 'been experiencing dry, hard stools or alternating between loose stools & constipation.'
   },
   {
     dosha: 'vata',
     section: 'digestive',
-    type: '',
+    type: 'appetite',
     text: 'been forgetting to eat or rushing through meals.'
   },
   {
     dosha: 'vata',
     section: 'reproductive',
-    type: '',
+    type: 'bowels',
     text: 'been experiencing bloating, gas or constipation around my menstrual period. '
   },
   {
     dosha: 'vata',
     section: 'reproductive',
-    type: '',
+    type: 'circulation',
     text: 'noticed my hands & feet are cold around my period'
   },
   {
     dosha: 'vata',
     section: 'reproductive',
-    type: '',
+    type: 'craving',
     text: 'been craving sugary foods around my period.'
   },
   {
     dosha: 'vata',
     section: 'reproductive',
-    type: '',
-    text: 'been feeling exhausted &/or extremely sensitive around my period '
+    type: 'manner',
+    text: 'been feeling exhausted &/or extremely sensitive around my period.'
   },
   {
     dosha: 'vata',
     section: 'reproductive',
-    type: '',
+    type: 'moods',
     text: 'been experiencing strong highs and lows in mood that can change rapidly.'
   },
   {
     dosha: 'vata',
     section: 'reproductive',
-    type: '',
+    type: 'secretions',
     text: 'been experiencing vaginal dryness.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
+    type: 'skin',
     text: 'been having skin problems that burn and/or itch, such as rashes, acne, and fever blisters.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
+    type: 'body',
     text: 'had inflammation in my joints or elsewhere in my body.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
+    type: 'temperature',
     text: 'been getting overheated very easily.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
+    type: 'eyes',
     text: 'been having bloodshot, itchy, or burning eyes.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
+    type: 'sleep',
     text: 'been waking up in the night overheated and/or I have night sweats.'
   },
   {
     dosha: 'pitta',
-    section: 'physical',
-    type: '',
+    section: 'digestive',
+    type: 'urine',
     text: 'my urine is dark yellow (without taking B-Vitamins), orange or red and it may burn.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
-    text: 'been experiencing stronger or sour body odor'
+    type: 'exercise',
+    text: 'been experiencing stronger or sour body odor.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
-    text: 'been experiencing a sour or metallic taste in my mouth and/or bad breath'
+    type: 'breath',
+    text: 'been experiencing a sour or metallic taste in my mouth and/or bad breath.'
   },
   {
     dosha: 'pitta',
     section: 'physical',
-    type: '',
-    text: 'having intense headaches in my temples, behind one or both eyes, at my sinuses or at the crown of my head'
+    type: 'head',
+    text: 'intense headaches in my temples, behind one or both eyes, at my sinuses or at the crown of my head.'
   },
   {
     dosha: 'pitta',
     section: 'mental',
-    type: '',
-    text: 'generally feeling impatient, irritable, and/or judgemental.'
+    type: 'feeling',
+    text: 'been generally feeling impatient, irritable, and/or judgemental.'
   },
   {
     dosha: 'pitta',
     section: 'mental',
-    type: '',
-    text: 'having vivid, violent or disturbing dreams. '
+    type: 'sleep',
+    text: 'vivid, violent or disturbing dreams. '
   },
   {
     dosha: 'pitta',
     section: 'mental',
-    type: '',
-    text: 'feeling frustrated at work with my own work & those around me'
+    type: 'work',
+    text: 'been feeling frustrated at work with my own work & those around me.'
   },
   {
     dosha: 'pitta',
     section: 'mental',
-    type: '',
-    text: 'told by my friends or partner that I am obsessed with my work'
+    type: 'work',
+    text: 'been told by my friends or partner that I am obsessed with my work.'
   },
   {
     dosha: 'pitta',
     section: 'mental',
-    type: '',
-    text: 'craving alcohol at the end of my workday'
+    type: 'craving',
+    text: 'been craving alcohol at the end of my workday.'
   },
   {
     dosha: 'pitta',
     section: 'digestive',
-    type: '',
-    text: 'Experiencing inflammation in my GI tract, acid reflux and/or ulcers.'
+    type: 'digestion',
+    text: 'been experiencing inflammation in my GI tract, acid reflux and/or ulcers.'
   },
   {
     dosha: 'pitta',
     section: 'digestive',
-    type: '',
-    text: 'craving meat, and spicy or fried food'
+    type: 'craving',
+    text: 'been craving meat, and spicy or fried food.'
   },
   {
     dosha: 'pitta',
     section: 'digestive',
-    type: '',
-    text: 'experiencing loose stools, diarrhea and/or burning elimination'
+    type: 'bowels',
+    text: 'been experiencing loose stools, diarrhea and/or burning elimination.'
   },
   {
     dosha: 'pitta',
     section: 'digestive',
-    type: '',
-    text: 'experiencing intense or gnawing hungry '
+    type: 'appetite',
+    text: 'been experiencing intense or gnawing hunger.'
   },
   {
     dosha: 'pitta',
     section: 'reproductive',
-    type: '',
-    text: 'having loose stools before or during my menstrual cycle'
+    type: 'bowels',
+    text: 'been having loose stools before or during my menstrual cycle.'
   },
   {
     dosha: 'pitta',
     section: 'reproductive',
-    type: '',
-    text: 'feeling more irritable or easy to anger before or during menstruation'
+    type: 'moods',
+    text: 'been feeling more irritable or easy to anger before or during menstruation.'
   },
   {
     dosha: 'pitta',
     section: 'reproductive',
-    type: '',
-    text: 'noticing more pimples or a rash around my menstrual cycle'
+    type: 'skin',
+    text: 'been noticing more pimples or a rash around my menstrual cycle.'
   },
   {
     dosha: 'pitta',
     section: 'reproductive',
-    type: '',
-    text: 'experiencing hot flashes. '
+    type: 'temperature',
+    text: 'been experiencing hot flashes. '
   },
 
   {
     dosha: 'pitta',
     section: 'reproductive',
-    type: '',
-    text: 'experiencing clots in my menstrual blood. '
+    type: 'secretions',
+    text: 'been experiencing clots in my menstrual blood. '
   },
   {
     dosha: 'pitta',
     section: 'reproductive',
-    type: '',
-    text: 'craving chocolate around my period'
+    type: 'cravings',
+    text: 'been craving chocolate around my period.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'experiencing sinus congestion, especially in the morning.'
+    type: 'mucus',
+    text: 'been experiencing sinus congestion, especially in the morning.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'waking up with a thick white coating on my tongue.'
+    type: 'mucus',
+    text: 'been waking up with a thick white coating on my tongue.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'experiencing water retention in my body, especially in my ankles or around my eyes.'
+    type: 'circulation',
+    text: 'been experiencing water retention in my body, especially in my ankles or around my eyes.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'gaining excess weight.'
+    type: 'weight',
+    text: 'been gaining excess weight.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'feeling tired, groggy and slow to waking in the morning even after getting a full nights sleep'
+    type: 'sleep',
+    text: 'been feeling tired, groggy and slow to waking in the morning even after getting a full nights sleep.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'experiencing lung congestion, cough and mucus.'
+    type: 'mucus',
+    text: 'been experiencing lung congestion, cough and mucus.'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'noticing that my limbs feel heavy, weak, or numb'
+    type: 'body',
+    text: 'been noticing that my limbs feel heavy, weak, or numb'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'my skin has looked and felt clammy'
+    type: 'skin',
+    text: 'been noticing that my skin has looked and felt clammy'
   },
   {
     dosha: 'kapha',
     section: 'physical',
-    type: '',
-    text: 'experiencing shortness of breath or labored breathing'
+    type: 'breath',
+    text: 'been experiencing shortness of breath or labored breathing'
   },
   {
     dosha: 'kapha',
     section: 'mental',
-    type: '',
-    text: 'feeling mentally dull. slow, and foggy.'
+    type: 'mind',
+    text: 'been feeling mentally dull. slow, and foggy.'
   },
   {
     dosha: 'kapha',
     section: 'mental',
-    type: '',
-    text: 'feeling stuck and lacking will power to change even though I know it is necessary.'
+    type: 'mind',
+    text: 'been feeling stuck and lacking will power to change even though I know it is necessary.'
   },
   {
     dosha: 'kapha',
     section: 'mental',
-    type: '',
-    text: 'feeling depressed and withdrawn.'
+    type: 'feeling',
+    text: 'been feeling depressed and withdrawn.'
   },
   {
     dosha: 'kapha',
     section: 'mental',
-    type: '',
-    text: 'regularly eating sweets or other comfort food to cope with stress and difficult emotions '
+    type: 'craving',
+    text: 'regularly been eating sweets or other comfort food to cope with stress and difficult emotions. '
   },
   {
     dosha: 'kapha',
     section: 'mental',
-    type: '',
-    text: 'having a desire to sleep more than is healthy'
+    type: 'sleep',
+    text: 'been having a desire to sleep more than is healthy.'
+  },
+  {
+    dosha: 'kapha',
+    section: 'digestive',
+    type: 'craving',
+    text: 'been experiencing fatigue and sugar cravings after I eat.'
+  },
+  {
+    dosha: 'kapha',
+    section: 'digestive',
+    type: 'appetite',
+    text: 'been experiencing a consistent lack of appetite.'
   },
   {
     dosha: 'kapha',
     section: 'digestive',
     type: '',
-    text: 'experiencing fatigue and sugar cravings after I eat.'
+    text: 'been experiencing bouts of a "sour" stomach or nausea.'
   },
   {
     dosha: 'kapha',
     section: 'digestive',
-    type: '',
-    text: 'experience a consistent lack of appetite'
+    type: 'bowels',
+    text: 'been having slow, soft, and or sticky bowel movements that feel incomplete.'
   },
   {
     dosha: 'kapha',
     section: 'digestive',
-    type: '',
-    text: 'experiencing bouts of a "sour" stomach or nausea'
-  },
-  {
-    dosha: 'kapha',
-    section: 'digestive',
-    type: '',
-    text: 'having slow, soft, and or sticky bowel movements that feel incomplete'
-  },
-  {
-    dosha: 'kapha',
-    section: 'digestive',
-    type: '',
-    text: 'Experiencing cloudy urine and difficulty emptying my bladder'
+    type: 'urine',
+    text: 'been experiencing cloudy urine and difficulty emptying my bladder.'
   },
   {
     dosha: 'kapha',
     section: 'reproductive',
-    type: '',
-    text: 'experiencing breast tenderness around menstruation.'
+    type: 'breasts',
+    text: 'been experiencing breast tenderness around menstruation.'
   },
   {
     dosha: 'kapha',
     section: 'reproductive',
-    type: '',
-    text: 'retaining water and feeling swoolen around my period. '
+    type: 'circulation',
+    text: 'been retaining water and feeling swollen around my period. '
   },
   {
     dosha: 'kapha',
     section: 'reproductive',
-    type: '',
-    text: 'increasingly dull and sluggish around my period.'
+    type: 'energy',
+    text: 'been increasingly dull and sluggish around my period.'
   },
   {
     dosha: 'kapha',
     section: 'reproductive',
-    type: '',
-    text: 'having a fishy smelling vaginal discharge. '
+    type: 'secretions',
+    text: 'been having a fishy smelling vaginal discharge. '
   },
   {
     dosha: 'kapha',
     section: 'reproductive',
-    type: '',
-    text: 'finding my metabolism has slowed post-menopause and that I gain weight more easily. '
+    type: 'energy',
+    text: 'been finding my metabolism has slowed post-menopause and that I gain weight more easily. '
   },
   {
     dosha: 'kapha',
     section: 'reproductive',
-    type: '',
-    text: 'diagnosed with a fibroids or ovarian cysts.'
+    type: 'body',
+    text: 'been diagnosed with a fibroids or ovarian cysts.'
   }
 ];
